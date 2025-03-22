bf33c2e7f49a9702ceb0d568b881f4f12ba5b4d8
Uttakarsh-01 <uttakarsh405@gmail.com>
2025-03-23 00:46:10 +0530

    project

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,7 +30,7 @@ app.use("/api/message", messageRoutes);
     app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
     app.get("*", (req,res) => {
-      res.sendFile(path.join(__dirname, "/frontend","dist","index.html"));
+      res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
     })
 
 
